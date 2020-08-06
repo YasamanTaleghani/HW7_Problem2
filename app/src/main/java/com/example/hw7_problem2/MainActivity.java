bd4ed93c33e27f89b7270c3019c084595694302e
304yasaman <y.taleghani@yahoo.com>
2020-08-06 22:15:18 +0430

    colors are set. text will be changed with click.

--- app/src/main/java/com/example/hw7_problem2/MainActivity.java
+++ app/src/main/java/com/example/hw7_problem2/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     TextView mIndigoTextView;
     TextView mVioletTextView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +27,9 @@ public class MainActivity extends AppCompatActivity {
         findViewMethod();
         setListeners();
 
-
     }
 
-    protected void findViewMethod(){
+    protected void findViewMethod() {
         mRedTextView = findViewById(R.id.RedtextView);
         mOrangeTextView = findViewById(R.id.OrangeTextView);
         mYellowTextView = findViewById(R.id.YellowTextView);
@@ -39,20 +39,55 @@ public class MainActivity extends AppCompatActivity {
         mVioletTextView = findViewById(R.id.VioletTextView);
     }
 
-    protected void setListeners(){
+    protected void setListeners() {
 
         mRedTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mRedTextView.setVisibility(View.INVISIBLE);
-                
+                mOrangeTextView.setTextColor(Color.RED);
+                mYellowTextView.setTextColor(Color.RED);
+                mGreenTextView.setTextColor(Color.RED);
+                mBlueTextView.setTextColor(Color.RED);
+                mIndigoTextView.setTextColor(Color.RED);
+                mVioletTextView.setTextColor(Color.RED);
+
+                mRedTextView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mOrangeTextView.setTextColor(Color.BLACK);
+                        mYellowTextView.setTextColor(Color.BLACK);
+                        mGreenTextView.setTextColor(Color.BLACK);
+                        mBlueTextView.setTextColor(Color.BLACK);
+                        mIndigoTextView.setTextColor(Color.BLACK);
+                        mVioletTextView.setTextColor(Color.BLACK);
+                    }
+                }, 5000);
             }
         });
 
         mOrangeTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               mOrangeTextView.setVisibility(View.INVISIBLE);
+                mOrangeTextView.setVisibility(View.INVISIBLE);
+                mRedTextView.setTextColor(Color.parseColor("#FF7417"));
+                mYellowTextView.setTextColor(Color.parseColor("#FF7417"));
+                mGreenTextView.setTextColor(Color.parseColor("#FF7417"));
+                mBlueTextView.setTextColor(Color.parseColor("#FF7417"));
+                mIndigoTextView.setTextColor(Color.parseColor("#FF7417"));
+                mVioletTextView.setTextColor(Color.parseColor("#FF7417"));
+
+                mOrangeTextView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mRedTextView.setTextColor(Color.BLACK);
+                        mYellowTextView.setTextColor(Color.BLACK);
+                        mGreenTextView.setTextColor(Color.BLACK);
+                        mBlueTextView.setTextColor(Color.BLACK);
+                        mIndigoTextView.setTextColor(Color.BLACK);
+                        mVioletTextView.setTextColor(Color.BLACK);
+                    }
+                }, 5000);
             }
         });
 
@@ -60,6 +95,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mYellowTextView.setVisibility(View.INVISIBLE);
+                mRedTextView.setTextColor(Color.YELLOW);
+                mOrangeTextView.setTextColor(Color.YELLOW);
+                mGreenTextView.setTextColor(Color.YELLOW);
+                mBlueTextView.setTextColor(Color.YELLOW);
+                mIndigoTextView.setTextColor(Color.YELLOW);
+                mVioletTextView.setTextColor(Color.YELLOW);
+
+                mYellowTextView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mRedTextView.setTextColor(Color.BLACK);
+                        mOrangeTextView.setTextColor(Color.BLACK);
+                        mGreenTextView.setTextColor(Color.BLACK);
+                        mBlueTextView.setTextColor(Color.BLACK);
+                        mIndigoTextView.setTextColor(Color.BLACK);
+                        mVioletTextView.setTextColor(Color.BLACK);
+                    }
+                }, 5000);
             }
         });
 
@@ -67,6 +120,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mGreenTextView.setVisibility(View.INVISIBLE);
+                mRedTextView.setTextColor(Color.GREEN);
+                mOrangeTextView.setTextColor(Color.GREEN);
+                mYellowTextView.setTextColor(Color.GREEN);
+                mBlueTextView.setTextColor(Color.GREEN);
+                mIndigoTextView.setTextColor(Color.GREEN);
+                mVioletTextView.setTextColor(Color.GREEN);
+
+                mGreenTextView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mRedTextView.setTextColor(Color.BLACK);
+                        mOrangeTextView.setTextColor(Color.BLACK);
+                        mYellowTextView.setTextColor(Color.BLACK);
+                        mBlueTextView.setTextColor(Color.BLACK);
+                        mIndigoTextView.setTextColor(Color.BLACK);
+                        mVioletTextView.setTextColor(Color.BLACK);
+                    }
+                }, 5000);
+
             }
         });
 
@@ -74,6 +146,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mBlueTextView.setVisibility(View.INVISIBLE);
+                mRedTextView.setTextColor(Color.BLUE);
+                mOrangeTextView.setTextColor(Color.BLUE);
+                mYellowTextView.setTextColor(Color.BLUE);
+                mGreenTextView.setTextColor(Color.BLUE);
+                mIndigoTextView.setTextColor(Color.BLUE);
+                mVioletTextView.setTextColor(Color.BLUE);
+
+                mBlueTextView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mRedTextView.setTextColor(Color.BLACK);
+                        mOrangeTextView.setTextColor(Color.BLACK);
+                        mYellowTextView.setTextColor(Color.BLACK);
+                        mGreenTextView.setTextColor(Color.BLACK);
+                        mIndigoTextView.setTextColor(Color.BLACK);
+                        mVioletTextView.setTextColor(Color.BLACK);
+                    }
+                }, 5000);
             }
         });
 
@@ -81,6 +171,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mGreenTextView.setVisibility(View.INVISIBLE);
+                mRedTextView.setTextColor(Color.GREEN);
+                mOrangeTextView.setTextColor(Color.GREEN);
+                mYellowTextView.setTextColor(Color.GREEN);
+                mBlueTextView.setTextColor(Color.GREEN);
+                mIndigoTextView.setTextColor(Color.GREEN);
+                mVioletTextView.setTextColor(Color.GREEN);
+
+                mGreenTextView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mRedTextView.setTextColor(Color.BLACK);
+                        mOrangeTextView.setTextColor(Color.BLACK);
+                        mYellowTextView.setTextColor(Color.BLACK);
+                        mBlueTextView.setTextColor(Color.BLACK);
+                        mIndigoTextView.setTextColor(Color.BLACK);
+                        mVioletTextView.setTextColor(Color.BLACK);
+                    }
+                }, 5000);
+
             }
         });
 
@@ -88,6 +197,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mBlueTextView.setVisibility(View.INVISIBLE);
+                mRedTextView.setTextColor(Color.BLUE);
+                mOrangeTextView.setTextColor(Color.BLUE);
+                mYellowTextView.setTextColor(Color.BLUE);
+                mGreenTextView.setTextColor(Color.BLUE);
+                mIndigoTextView.setTextColor(Color.BLUE);
+                mVioletTextView.setTextColor(Color.BLUE);
+
+                mBlueTextView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mRedTextView.setTextColor(Color.BLACK);
+                        mOrangeTextView.setTextColor(Color.BLACK);
+                        mYellowTextView.setTextColor(Color.BLACK);
+                        mGreenTextView.setTextColor(Color.BLACK);
+                        mIndigoTextView.setTextColor(Color.BLACK);
+                        mVioletTextView.setTextColor(Color.BLACK);
+                    }
+                }, 5000);
             }
         });
 
@@ -95,6 +222,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mIndigoTextView.setVisibility(View.INVISIBLE);
+                mRedTextView.setTextColor(Color.parseColor("#810288"));
+                mOrangeTextView.setTextColor(Color.parseColor("#810288"));
+                mYellowTextView.setTextColor(Color.parseColor("#810288"));
+                mGreenTextView.setTextColor(Color.parseColor("#810288"));
+                mBlueTextView.setTextColor(Color.parseColor("#810288"));
+                mVioletTextView.setTextColor(Color.parseColor("#810288"));
+
+                mIndigoTextView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mRedTextView.setTextColor(Color.BLACK);
+                        mOrangeTextView.setTextColor(Color.BLACK);
+                        mYellowTextView.setTextColor(Color.BLACK);
+                        mGreenTextView.setTextColor(Color.BLACK);
+                        mBlueTextView.setTextColor(Color.BLACK);
+                        mVioletTextView.setTextColor(Color.BLACK);
+                    }
+                }, 5000);
             }
         });
 
@@ -102,6 +247,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mVioletTextView.setVisibility(View.INVISIBLE);
+                mRedTextView.setTextColor(Color.parseColor("#E81FD2"));
+                mOrangeTextView.setTextColor(Color.parseColor("#E81FD2"));
+                mYellowTextView.setTextColor(Color.parseColor("#E81FD2"));
+                mGreenTextView.setTextColor(Color.parseColor("#E81FD2"));
+                mBlueTextView.setTextColor(Color.parseColor("#E81FD2"));
+                mVioletTextView.setTextColor(Color.parseColor("#E81FD2"));
+
+                mVioletTextView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mRedTextView.setTextColor(Color.BLACK);
+                        mOrangeTextView.setTextColor(Color.BLACK);
+                        mYellowTextView.setTextColor(Color.BLACK);
+                        mGreenTextView.setTextColor(Color.BLACK);
+                        mBlueTextView.setTextColor(Color.BLACK);
+                        mIndigoTextView.setTextColor(Color.BLACK);
+                    }
+                }, 5000);
             }
         });
 
